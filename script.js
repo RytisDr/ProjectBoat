623aa31090f99143ad9bff295111250d83baac27
Sniedze <u.sniedze@gmail.com>
2017-11-28 21:45:54 +0100

    Created project.html

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let boat = document.querySelector("#boat");
+let button1 = document.querySelector("#im1");
+let button2 = document.querySelector("#im2");
+let button3 = document.querySelector("#im3");
+let button4 = document.querySelector("#im4");
+let button5 = document.querySelector("#im5");
+let text1 = document.querySelector("#p1");
+
 window.addEventListener('load', start);
-function start(){
+
+function start() {
     boat.classList.add("opacity");
+}
+
+button1.addEventListener("click", beClicked);
+
+function beClicked() {
+    document.getElementById("p1").style.display = "block";
+}
+text1.addEventListener("click", text1Clicked);
+
+function text1Clicked() {
+    document.getElementById("p1").style.display = "none";
 }
