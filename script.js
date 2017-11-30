fecee1b00a35b4cd36e070454e2c4d3f62e062d2
RytisDr <r.drazdauskas@gmail.com>
2017-11-30 21:09:32 +0100

    Merged, still needs fixing.

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 let boat = document.querySelector("#boat");
-let button1 = document.querySelector("#im1");
-let button2 = document.querySelector("#im2");
-let button3 = document.querySelector("#im3");
-let button4 = document.querySelector("#im4");
-let button5 = document.querySelector("#im5");
-let text1 = document.querySelector("#p1");
+let transparentBoat = document.querySelector("#logoTransparent")
 
-button1.addEventListener("click", beClicked);
+window.addEventListener('load', start);
 
-function beClicked() {
-    document.getElementById("p1").style.display = "block";
-}
-text1.addEventListener("click", text1Clicked);
+function start() {
+    transparentBoat.classList.add("float");
 
-function text1Clicked() {
-    document.getElementById("p1").style.display = "none";
+
 }
