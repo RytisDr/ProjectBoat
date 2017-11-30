122775a666590b935d63368216f55790151821d3
Sniedze <u.sniedze@gmail.com>
2017-11-30 19:13:24 +0100

    commit

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
 let boat = document.querySelector("#boat");
-let button1 = document.querySelector("#im1");
-let button2 = document.querySelector("#im2");
-let button3 = document.querySelector("#im3");
-let button4 = document.querySelector("#im4");
-let button5 = document.querySelector("#im5");
-let text1 = document.querySelector("#p1");
+let transparentBoat = document.querySelector("#logoTransparent")
 
 window.addEventListener('load', start);
 
 function start() {
-    boat.classList.add("opacity");
-}
+    transparentBoat.classList.add("float");
 
-button1.addEventListener("click", beClicked);
 
-function beClicked() {
-    document.getElementById("p1").style.display = "block";
-}
-text1.addEventListener("click", text1Clicked);
-
-function text1Clicked() {
-    document.getElementById("p1").style.display = "none";
 }
